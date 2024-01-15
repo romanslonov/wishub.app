4f3fd02402fdb14f100c021b3378f9c78f8873a2
Roman Slonov <destabiliseus@gmail.com>
2024-01-15 21:21:01 +0800

    chore: clean up navigation

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,7 +1,7 @@
 import { cn } from "~/lib/cn";
 import { UserMenu } from "./user-menu";
 import { buttonVariants } from "~/components/ui/button";
-import { CheckCircle, Ghost, ListPlus, XCircle } from "lucide-react";
+import { Ghost, ListPlus } from "lucide-react";
 import { User } from "lucia";
 import { Link } from "@remix-run/react";
 
@@ -15,11 +15,6 @@ export function Navigation({ user }: { user: User }) {
         <div className="flex items-center gap-4">
           {user ? (
             <>
-              {user.emailVerified ? (
-                <CheckCircle size={16} className="text-emerald-500" />
-              ) : (
-                <XCircle size={16} className="text-rose-500" />
-              )}
               <Link
                 className={cn(
                   buttonVariants({ size: "sm", variant: "secondary" })
