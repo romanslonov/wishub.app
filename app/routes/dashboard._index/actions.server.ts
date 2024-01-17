b5b4b1fead258ad0b05f45c386c4ca9e94df171f
Roman Slonov <destabiliseus@gmail.com>
2024-01-17 11:40:53 +0800

    Revert "chore: slowdown for testing purpose"
    This reverts commit ed640a8e51e7b2ff6b00e0b8de7b90b6d22f4334.

--- app/routes/dashboard._index/actions.server.ts
+++ app/routes/dashboard._index/actions.server.ts
@@ -1,10 +1,6 @@
 import { prisma } from "~/lib/prisma.server";
 
-const slowdown = (ms: number) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
-
 export async function getLists(ownerId: string) {
-  await slowdown(1000);
   return prisma.list.findMany({
     where: { ownerId: ownerId },
     orderBy: { createdAt: "desc" },
