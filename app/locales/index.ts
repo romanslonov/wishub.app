49ae20db4aef11e0bf1642ab0e838c11ed7cd71b
Roman Slonov <destabiliseus@gmail.com>
2024-01-24 10:22:39 +0800

    chore: debug

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,6 +1,10 @@
 export type Locale = "en" | "ru";
 
 export function getLocaleFromRequest(request: Request): Locale {
+  console.log(
+    request.headers.get("current-locale"),
+    Object.fromEntries(request.headers.entries())
+  );
   return (request.headers.get("current-locale") as "ru" | undefined) || "en";
 }
 
