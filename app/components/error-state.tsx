18ad95fddae1db7a06a784279f9e7011d2c2f31b
Roman Slonov <destabiliseus@gmail.com>
2024-02-22 15:02:27 +0800

    fix: error-state

--- app/components/error-state.tsx
+++ app/components/error-state.tsx
@@ -3,9 +3,7 @@ import { CircleSlash } from "lucide-react";
 import { type LocaleData } from "~/locales";
 
 export function ErrorState() {
-  const data = useRouteLoaderData<{ t: LocaleData }>(
-    "routes/dashboard.lists.$id"
-  );
+  const data = useRouteLoaderData<{ t: LocaleData }>("root");
   return (
     <div className="flex items-center justify-center flex-col py-16 space-y-4">
       <CircleSlash className="w-16 h-16 text-red-500" />
