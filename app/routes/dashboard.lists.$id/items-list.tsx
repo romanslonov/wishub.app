50ae33c5e0082cd685b796f4a5c1807a49796598
Roman Slonov <destabiliseus@gmail.com>
2024-02-24 13:47:14 +0800

    feat: add empty state for shared list

--- app/routes/dashboard.lists.$id/items-list.tsx
+++ app/routes/dashboard.lists.$id/items-list.tsx
@@ -30,11 +30,11 @@ export function ItemsList({ items }: { items: Item[] }) {
     return (
       <div className="border border-dashed bg-card rounded-2xl py-16 text-center">
         <div className="relative mb-8 h-48">
-          <div className="absolute shadow-md top-0 left-[58%] sm:left-[53%] translate-x-[-53%] border w-32 h-48 rounded-xl bg-background transform rotate-12 mx-auto p-4 space-y-2">
+          <div className="absolute shadow-sm top-0 z-[2] left-[58%] sm:left-[53%] translate-x-[-53%] border w-32 h-48 rounded-xl bg-background transform rotate-12 mx-auto p-4 space-y-2">
             <div className="bg-gradient-to-b from-muted to-muted/50 h-16 rounded-md w-full"></div>
             <div className="bg-gradient-to-b from-muted to-muted/50 h-16 rounded-md w-full"></div>
           </div>
-          <div className="absolute shadow-sm top-0 left-[37%] sm:left-[45%] translate-x-[-45%] border w-24 h-40 rounded-xl bg-background transform -rotate-12 -z-10 mx-auto p-4 space-y-2">
+          <div className="absolute shadow-sm top-0 left-[37%] sm:left-[45%] translate-x-[-45%] border w-24 h-40 rounded-xl bg-background transform -rotate-12 z-[1] mx-auto p-4 space-y-2">
             <div className="bg-gradient-to-b from-muted to-muted/50 h-12 rounded-md w-full"></div>
             <div className="bg-gradient-to-b from-muted to-muted/50 h-12 rounded-md w-full"></div>
           </div>
--- app/routes/s.$id/items-list.tsx
+++ app/routes/s.$id/items-list.tsx
@@ -21,6 +21,30 @@ interface ItemsListProps extends HTMLAttributes<HTMLUListElement> {
 
 export function ItemsList({ items, isMyself, user, ...props }: ItemsListProps) {
   const data = useRouteLoaderData<{ t: LocaleData }>("routes/s.$id");
+
+  if (items.length === 0) {
+    return (
+      <div className="border border-dashed bg-card rounded-2xl py-16 text-center">
+        <div className="relative mb-8 h-48">
+          <div className="absolute shadow-sm z-[2] top-0 left-[58%] sm:left-[53%] translate-x-[-53%] border w-32 h-48 rounded-xl bg-background transform rotate-12 mx-auto p-4 space-y-2">
+            <div className="bg-gradient-to-b from-muted to-muted/50 h-16 rounded-md w-full"></div>
+            <div className="bg-gradient-to-b from-muted to-muted/50 h-16 rounded-md w-full"></div>
+          </div>
+          <div className="absolute shadow-sm top-0 left-[37%] sm:left-[45%] translate-x-[-45%] border w-24 h-40 rounded-xl bg-background transform -rotate-12 z-[1] mx-auto p-4 space-y-2">
+            <div className="bg-gradient-to-b from-muted to-muted/50 h-12 rounded-md w-full"></div>
+            <div className="bg-gradient-to-b from-muted to-muted/50 h-12 rounded-md w-full"></div>
+          </div>
+        </div>
+        <h2 className="text-xl font-bold mb-2 tracking-tight">
+          {data?.t.shared.list.empty.title}
+        </h2>
+        <p className="text-center text-muted-foreground">
+          {data?.t.shared.list.empty.subtitle}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <ul className={props.className}>
       {items.map((item) => (
