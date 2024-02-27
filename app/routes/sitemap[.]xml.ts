daac3daffb8de8d15152a5656bbf13c456ae566f
Roman Slonov <destabiliseus@gmail.com>
2024-02-27 10:26:39 +0800

    feat: add simple sitemap

---
+++ app/routes/sitemap[.]xml.ts
@@ -0,0 +1,19 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { xml } from "remix-utils/responses";
+
+export function loader({ request }: LoaderFunctionArgs) {
+  const origin = new URL(request.url).origin;
+
+  const urls = [{ loc: `${origin}/` }];
+
+  return xml(
+    `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${[
+      ...urls,
+    ].map((url) => `<url><loc>${url.loc.toString()}</loc></url>`)}</urlset>`,
+    {
+      headers: {
+        "Cache-Control": `public, max-age=${60 * 5}`,
+      },
+    }
+  );
+}
